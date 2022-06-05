cf3d2f2624c362606389a7f07a27b1901e185d2f
AnkushRawat <ankushrwt344@gmail.com>
2022-06-05 20:02:26 +0530

    Uploading NewContacts file in Uc2 branch

---
+++ RFP_DAY23_AddressBooKSystem/NewContact_UC2.cs
@@ -0,0 +1,40 @@
+﻿using RFP_DAY_23_ADDRESSBOOK_Lambda_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DAY_23_ADDRESSBOOK_Lambda_
+{
+    public class NewContacts
+    {
+        CreateContacts obj = new CreateContacts();
+        public void newContacts()
+        {
+            Console.WriteLine("Enter the number of contacts You want to add");
+            int num = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i<=num;i++)
+            {
+                obj.CreateContactMethod();
+            }
+        }
+        public void printcont()
+        {
+            foreach(var a in obj.ContactBook)
+            {
+                Console.WriteLine();
+                Console.WriteLine("FirstName is "+ a.firstName);
+                Console.WriteLine("LastName is "+ a.lastName);
+                Console.WriteLine("Address is "+ a.address);
+                Console.WriteLine("City is " + a.city);
+                Console.WriteLine("State is "+ a.state);
+                Console.WriteLine("Zip is "+ a.zip);
+                Console.WriteLine("PhoneNumber is "+ a.phone);
+                Console.WriteLine("Email is "+a.email);
+                
+            }
+        }
+        
+    }
+}
--- RFP_DAY23_AddressBooKSystem/Program.cs
+++ RFP_DAY23_AddressBooKSystem/Program.cs
@@ -1,6 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY_23_ADDRESSBOOK_Lambda_;
+
+
 Console.WriteLine("WELCOME TO ADDRESS BOOK PROGRAM ");
-CreateContacts obj1 = new CreateContacts();
-obj1.CreateContactMethod();
-obj1.print();
+
+Console.WriteLine("ENTER \n1 FOR CREATE CONTACTS PROGRAM \n2 FOR ADDING NEW CONTACTS PROGRAM");
+int number = Int32.Parse(Console.ReadLine());
+switch(number)
+{
+    case 1:
+        CreateContacts obj1 = new CreateContacts();
+        obj1.CreateContactMethod();
+        obj1.print();
+        break;
+
+    case 2:
+        NewContacts obj2 = new NewContacts();
+        obj2.newContacts();
+        obj2.printcont();
+        break;
+}
